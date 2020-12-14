cbc84d6ed53322b12fd9a1c6398721733beab8c4
Roseanne Nguyen <rtnguye1@gmail.com>
2020-12-14 14:59:38 -0700

    updates

--- app.js
+++ app.js
@@ -1,69 +1,80 @@
+
+
 $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm a"))
 
 
 $(document).ready(function () {
 
-    $(".saveBtn").on("click", function() {
 
-        var taskText = $(this).siblings(".textarea").val();
-        var taskHour = $(this).siblings(".textarea").attr("id");
-        
-        localStorage.setItem(taskText, taskHour);
-       
+    $(".saveBtn").on("click", function (event) {
+        event.preventDefault()
+
+        var taskText = $(this).prev(".textarea").val().trim();
+        var taskHour = $(this).prev().attr("id");
+
+        // console.log(document.querySelectorAll (".textarea"))
+
+        localStorage.setItem(taskText, taskHour)
+
+ 
+
+
+        // savedData = JSON.parse(localStorage.getItem(taskText))
+
+
+
+
+        // saveText();
+        // storedData();
 
     })
 
+    // function saveText() {
+    //     var storedText = JSON.parse(localStorage.getItem("taskText"))
 
-    $("#hour-9 .textarea").val(localStorage.getItem("hour-9"));
-    $("#hour-10 .textarea").val(localStorage.getItem("hour-10"));
-    $("#hour-11 .textarea").val(localStorage.getItem("hour-11"));
-    $("#hour-12 .textarea").val(localStorage.getItem("hour-12"));
-    $("#hour-1 .textarea").val(localStorage.getItem("hour-1"));
-    $("#hour-2 .textarea").val(localStorage.getItem("hour-2"));
-    $("#hour-3 .textarea").val(localStorage.getItem("hour-3"));
-    $("#hour-4 .textarea").val(localStorage.getItem("hour-4"));
-    $("#hour-5 .textarea").val(localStorage.getItem("hour-5"));
-    $("#hour-6 .textarea").val(localStorage.getItem("hour-6"));
-    $("#hour-7 .textarea").val(localStorage.getItem("hour-7"));
+    //     if (storedText !== null) {
+    //         taskText = storedText;
+    //     }
+    // }
 
-    var currentHour = moment().hour();
-    
+    // function storedData() {
+    //     localStorage.setItem("taskText", JSON.stringify(taskText));
+    // }
+
+
+
+
+
     function currentTime() {
 
-      
+        var currentHour = moment().hour();
 
         $(".time-block").each(function () {
-            var blockTime = $(this).attr("id");
-            var blockHour = parseInt(blockTime)
-            // console.log(blockTime, currentHour)
 
-            if (blockHour < currentHour) {
-                $(this).addClass("past");
-               
-                
+            var blockTime = parseInt($(this).attr("id").split("-")[1]);
+
+
+            if (blockTime < currentHour) {
+                $(this).find("textarea").addClass("past");
+
+
             }
-            else if (blockHour === currentHour) {
-                $(this).addClass("present");
-            
-                
+            else if (blockTime === currentHour) {
+                $(this).find("textarea").addClass("present");
+
+
 
             }
             else {
-                $(this).addClass("future");
-             
-                
+                $(this).find("textarea").addClass("future");
+
+
 
             }
-
-
         })
-
     }
- 
 
     currentTime();
-    renderStoredInputs()
-
 
 })
 
